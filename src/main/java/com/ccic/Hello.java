d6087ab5355066aae3adcb5da3cbcd08d56eab17
jackson <573694655@qq.com>
2020-06-13 16:17:02 +0800

    Initial commit 第一次提交

---
+++ src/main/java/com/ccic/Hello.java
@@ -0,0 +1,13 @@
+package com.ccic;
+
+/**
+ * @quthor hzs
+ * @create 06-13-2020
+ * @time 16:15
+ * @description 功能描述
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
